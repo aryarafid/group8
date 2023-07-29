636723731441e92b810b4d20ee407213effc02a3
aryarafid <aryamohamaddanendra@gmail.com>
2023-07-30 00:41:47 +0700

    create cashier good

--- projects/server/Controllers/adminController.js
+++ projects/server/Controllers/adminController.js
@@ -3,6 +3,9 @@ const path = require("path");
 const user = db.User;
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const {
+  now
+} = require("sequelize/types/utils");
 require("dotenv").config({
   path: path.resolve("../.env"),
 });
@@ -10,64 +13,126 @@ require("dotenv").config({
 const adminController = {
   createCashier: async (req, res) => {
     try {
-      const { username, email, password, role, isActive } = req.body;
-      
-      res.status(200).json({ message: "create cashier success", data: token });
+      const {
+        username,
+        email,
+        password
+      } = req.body;
+
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(password, salt);
+
+      await db.sequelize.transaction(async (t) => {
+        const cashCreate = await user.create({
+          username,
+          email,
+          password: hashPassword,
+          role: "Cashier",
+          isActive: true,
+        }, {
+          transaction: t
+        });
+      });
+      res.status(200).json({
+        message: "create cashier success",
+        data: token
+      });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({
+        message: error.message
+      });
     }
   },
 
   updateCashier: async (req, res) => {
     try {
-      const { username, email, password, role, isActive } = req.body;
-      let account = {};
-      if (username) {
-        account.username = username;
+      const {
+        id
+      } = req.params;
+      const {
+        username,
+        email,
+        password,
+        role,
+      } = req.body;
+
+      const userFind = await user.findByPk(id);
+      if (userFind) {
+        // const newValues = {}
+        if (username) userFind.username = username;
+        if (email) userFind.email = email;
+        if (password) {
+          const salt = await bcrypt.genSalt(10);
+          password = await bcrypt.hash(password, salt);
+          userFind.password = password
+        }
+        if (role) userFind.role = role;
+        userFind.updatedAt = Date()
+        return res.json(userFind)
+        if (Object.keys(newValues).length > 0) {
+          await db.sequelize.transaction(async (t) => {
+            await user.update({
+              userFind
+            }, {
+              where: {
+                id: id,
+              },
+            }, {
+              transaction: t
+            });
+            // return res.json(userFind);
+            return res
+              .status(200)
+              .json({
+                message: "update cashier success",
+              });
+          })
+        } else {
+          res.status(400).json({
+            message: "cant be updated if every input is empty"
+          });
+        }
+      } else {
+        res.status(404).json({
+          message: "user not found"
+        });
       }
-      if (email) {
-        account.email = email;
-      }
-      const checkLogin = await user.findOne({ where: account });
-      if (!checkLogin)
-        return res.status(500).json({ message: "Account not defined" });
-      const checkPassword = await bcrypt.compare(password, checkLogin.password);
-      if (!checkPassword)
-        return res.status(500).json({ message: "Password wrong" });
-      let payload = {
-        id: checkLogin.id,
-        username: checkLogin.username,
-      };
-      const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: "3h" });
-      res.status(200).json({ message: "Login Success", data: token });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({
+        message: error.message
+      });
     }
   },
+
   deleteCashier: async (req, res) => {
     try {
-      const { username, email, password, role, isActive } = req.body;
-      let account = {};
-      if (username) {
-        account.username = username;
+      const {
+        id
+      } = req.params;
+
+      const userFind = await user.findByPk(id);
+      if (userFind) {
+        await db.sequelize.transaction(async (t) => {
+          userFind.isActive = false
+          await user.save({
+            transaction: t
+          })
+          return res
+            .status(200)
+            .json({
+              message: "delete cashier success",
+            });
+        })
+
+      } else {
+        res.status(404).json({
+          message: "user not found"
+        });
       }
-      if (email) {
-        account.email = email;
-      }
-      const checkLogin = await user.findOne({ where: account });
-      if (!checkLogin)
-        return res.status(500).json({ message: "Account not defined" });
-      const checkPassword = await bcrypt.compare(password, checkLogin.password);
-      if (!checkPassword)
-        return res.status(500).json({ message: "Password wrong" });
-      let payload = {
-        id: checkLogin.id,
-        username: checkLogin.username,
-      };
-      const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: "3h" });
-      res.status(200).json({ message: "Login Success", data: token });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({
+        message: error.message
+      });
     }
   },
 };
--- projects/server/Routers/adminRouter.js
+++ projects/server/Routers/adminRouter.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../Controllers/adminController");
+const {
+  loginValidator,
+  validateRegist,
+  createCashierValidator,
+} = require("../middleware/Validator");
+const {
+  authValidator
+} = require("../middleware/authValidator");
 
-router.post("/create-cashier", adminController.createCashier);
-router.post("/update-cashier/:id", adminController.updateCashier);
-router.post("/delete-cashier/:id", adminController.deleteCashier);
+router.post(
+  "/create-cashier",
+  createCashierValidator,
+  validateRegist,
+  authValidator.verifyToken,
+  adminController.createCashier
+);
+router.patch(
+  "/update-cashier/:id",
+  // authValidator.verifyToken,
+  adminController.updateCashier
+);
+router.delete("/delete-cashier/:id",
+  // authValidator.verifyToken,
+  adminController.deleteCashier);
 
 module.exports = router;
