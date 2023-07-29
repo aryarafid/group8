19724abc46f446c2eff38468ab83d59f7b087cf0
Muh. Afdal Maulana Said <68625050+afdalmaulana@users.noreply.github.com>
2023-07-29 23:23:00 +0700

    DEV-11 done (#10)

--- projects/client/src/components/Landing/Home.jsx
+++ projects/client/src/components/Landing/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Login from "./Login";
 import { useSelector } from "react-redux";
 import DashBoardCashier from "./dashBoardCashier";
+import DashBoardAdmin from "./dashBoardAdmin";
 
 export default function Home() {
   // const [login, setLogin] = useState(true);
@@ -14,10 +15,10 @@ export default function Home() {
       <main>
         {!login ? (
           <Login />
-        ) : user.role === "Cashier" ? (
-          <DashBoardCashier />
+        ) : user.role === "Admin" ? (
+          <DashBoardAdmin />
         ) : (
-          <></>
+          <DashBoardCashier />
         )}
       </main>
     </>
