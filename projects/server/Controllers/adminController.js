3eec2bc5e9f6dd470cb33ffac1cdb458fa567300
aryarafid <aryamohamaddanendra@gmail.com>
2023-07-30 00:41:47 +0700

    create cashier good

--- projects/server/Controllers/adminController.js
+++ projects/server/Controllers/adminController.js
@@ -3,6 +3,9 @@ const path = require("path");
 const user = db.User;
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const {
+  now
+} = require("sequelize/types/utils");
 require("dotenv").config({
   path: path.resolve("../.env"),
 });
